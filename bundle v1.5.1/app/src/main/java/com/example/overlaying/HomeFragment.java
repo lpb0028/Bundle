d7f81e20a4e6143898c213436c0889b364ae8068
Logan Banyai <lpb0028@auburn.edu>
2025-07-13 09:11:33 -0500

    Version 1.5.1-experimental

---
+++ bundle v1.5.1/app/src/main/java/com/example/overlaying/HomeFragment.java
@@ -0,0 +1,43 @@
+package com.example.overlaying;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        loadButtons(view);
+
+        return view;
+    }
+
+    private void loadButtons(View view) {
+        Activity activity = (Activity) getActivity();
+        assert activity != null;
+
+        // Start service and end activity
+        view.findViewById(R.id.home_button1).setOnClickListener(v -> {
+            if (Manager.boundToService) {
+                Manager.binder.StartAdventure();
+                activity.close(false);
+            }
+            else
+                activity.manager.bindToService(activity);
+        });
+
+        // End service, end activity; Exit Application
+        view.findViewById(R.id.home_button2).setOnClickListener(v -> {
+            if (Manager.boundToService) {
+                Manager.binder.StopAdventure();
+                Manager.binder.StopService();
+            }
+            activity.close(true);
+        });
+    }
+}
---
+++ bundle v1.5.1/app/src/main/java/com/example/overlaying/ToastManager.java
@@ -0,0 +1,71 @@
+package com.example.overlaying;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ToastManager {
+    private final Context context;
+    private final Queue<CharSequence> toastQueue = new LinkedList<>();
+    private boolean isShowing = false; // Message currently showing?
+
+    // Queue and Handler to manage timings
+    private final Queue<Integer> durationQueue = new LinkedList<>();
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
+    public ToastManager(Context context) {
+        this.context = context;
+    }
+
+    public void Close(Context context) {}
+
+    // Add a toast to queue (or if queue empty display message now)
+    public void sendToast(CharSequence message, int duration) {
+        toastQueue.add(message);
+        durationQueue.add(duration);
+        if (!isShowing)
+            showNextToast();
+    }
+
+    // Show next message in queue (returns if queue empty)
+    private void showNextToast() {
+        // Get message and ensure non-null
+        CharSequence message = toastQueue.poll();
+        if (message == null) return;
+
+        // Schedule next message with handler to ensure run on UI thread
+        handler.post(() -> {
+            isShowing = true;
+
+            // Create text view
+            TextView text = new TextView(context);
+            text.setText(message);
+            text.setBackgroundResource(R.drawable.rounded_background);
+            text.setTextColor(Color.BLACK);
+
+            // Create toast
+            Toast toast = new Toast(context);
+            toast.setView(text);
+            toast.show();
+
+            // Remove on delay and show next (if applicable)
+            handler.postDelayed(() -> {
+                toast.cancel();
+                isShowing = false;
+                if (!toastQueue.isEmpty()) {
+                    showNextToast();
+                }
+            }, (durationQueue.poll())); // Duration of the toast
+        });
+
+    }
+}
